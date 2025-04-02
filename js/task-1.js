4d91002f4847d3930b90ab813a0ab5ececaccb3a
liliana_toiunda <liliana.ntw@gmail.com>
2025-04-02 21:49:29 +0300

    add task - 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,32 +1,67 @@
-// Напиши функцію isEnoughCapacity(products, containerSize),
-// яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+// Задача 1. Імена користувачів //метод map()
+// Напиши стрелочную функцию getUserNames(users), которая принимает один
+// параметр users — массив объектов пользователей. Функция должна возвращать
+// массив имен всех пользователей (свойство name) из массива users.
 
-// Функція оголошує два параметри:
+// Проверка:
+// Возьми код ниже и вставь после объявления своей функции для проверки ее
+// корректности. В консоль будут выведены результаты ее вызовов.
 
-// products — об’єкт, у якому ключі містять назви товарів,
-// а їхні значення — кількість цих товарів.Наприклад, { apples: 2, grapes: 4 }.
-// containerSize — число, максимальна кількість одиниць товарів,
-// яку в себе може вмістити контейнер.
-// Функція має повернути результат перевірки,
-// чи помістяться всі товари в контейнер.Тобто порахувати загальну кількість товарів
-//  в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один
+// параметр users — масив об’єктів користувачів. Функція має повертати
+// масив імен усіх користувачів (властивість name) із масиву users.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки
+// коректності її роботи. У консоль будуть виведені результати її викликів.
+// Залиш цей код для перевірки ментором.
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
-//  У консоль будуть виведені результати її викликів.
+// На що буде звертати увагу ментор при перевірці:
+// Оголошена змінна getUserNames
+// Змінній getUserNames присвоєна стрілочна функція з параметром (users).
+// Для перебирання параметра users використовується метод map()
+// Виклик функції із зазначеним масивом користувачів повертає масив
+// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head",
+// "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// Виклик функції з випадковими, але валідними аргументами повертає
+// правильне значення
 
-function isEnoughCapacity(products, containerSize) {
-  let quantity = 0;
-  const values = Object.values(products);
-  for (let i = 0; i < values.length; i += 1) {
-    quantity += values[i];
-  }
-  return quantity <= containerSize;
-}
+const getUserNames = (users) => users.map((user) => user.name);
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764,
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,44 +1,104 @@
-// Об’єкт profile описує профіль користувача на ігровій платформі.
-// У його властивостях зберігається ім’я профілю username
-// та кількість активних годин playTime, проведених у грі.
-// const profile = {
-//     username: "Jacob",
-//     playTime: 300,
-// };
-// Доповни об’єкт profile методами для роботи з його властивостями.
+// Задача 3. Сортування за кількістю друзів // метод toSorted()
 
-// Метод changeUsername(newName) повинен приймати рядок (нове ім’я)
-// в параметр newName та змінювати значення властивості username
-// на нове. Нічого не повертає.
+// Напиши стрілочну функцію sortByDescendingFriendCount(users),
+//  яка прийматиме один параметр users — масив об’єктів користувачів.
 
-// Метод updatePlayTime(hours) повинен приймати число (кількість годин)
-// у параметр hours та збільшити на нього значення властивості playTime.
-// Нічого не повертає.
+// Функція має повертати масив усіх користувачів,
+//  відсортованих за спаданням кількостій
+//  їх друзів (властивість friends).
 
-// Метод getInfo() має повертати рядок формату
-// <Username> has <amount> active hours!,
-// де <Username> — це ім’я профілю,
-// а <amount> — кількість ігрових годин.
+// Візьми код нижче і встав після оголошення
+//  своєї функції для перевірки коректності її роботи.
+//  У консоль будуть виведені результати її роботи.
+// На що буде звертати увагу ментор при перевірці:
 
-const profile = {
-  username: "Jacob",
-  playTime: 300,
-  changeUsername(newName) {
-    this.username = newName;
-  },
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-  },
-};
+// Оголошена змінна sortByDescendingFriendCount
+// Змінній sortByDescendingFriendCount присвоєна
+//  стрілочна функція з параметром (users)
+// Для перебирання параметра users використаний
+//  метод toSorted()
+// Виклик функції із зазначеним масивом users повертає
+//  новий масив користувачів, відсортований
+//  за спаданням кількості їхніх друзів
+// Виклик функції з випадковими,
+//  але валідними аргументами повертає
+//  правильне значення
+//---------------------------------------------------------
+//---------------------------------------------------------
 
-// Проверка
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male",
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female",
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male",
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female",
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male",
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male",
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female",
+    },
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
